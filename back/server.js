cc639aaee513dbab7f8d18df42ff105befd8c57c
ahmad-dev-code <22230512@students.liu.edu.lb>
2025-12-31 07:01:47 +0200

    adding backend

--- back/server.js
+++ back/server.js
@@ -5,7 +5,13 @@ const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(''));
+app.use(cors(
+    {
+        origin: 'https://transcendent-klepon-55987f.netlify.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 app.use(express.json());
 
 // Login
